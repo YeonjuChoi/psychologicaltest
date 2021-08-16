4849b8137b8ba2d7c460e5572190d62b9905316a
[2기-1반] 최연주 <jujubee6323@gmail.com>
2021-08-16 21:21:56 +0900

    react-router-dom, components, styled etc

--- src/App.js
+++ src/App.js
@@ -1,10 +1,40 @@
-import logo from './logo.svg';
 import './App.css';
+import { BrowserRouter, Switch, Route } from 'react-router-dom';
+import styled from 'styled-components';
+import Main from './components/Main';
+import Questions from './components/Questions';
+import Result from './components/Result';
 
 function App() {
+  const MainDiv = styled.div`
+    background-color: #FDF6F0;
+    max-width: 780px;
+    text-align: center;
+    min-height: 900px;
+    border-radius: 30px;
+    margin: auto;
+    padding: 20px;
+    display: flex;
+    align-items: center;
+    flex-direction: column;
+`;
   return (
     <div className="App">
-      
+      <MainDiv>
+        <BrowserRouter>
+          <Switch>
+            <Route path='/' exact>
+              <Main />
+            </Route>
+            <Route path='/questions'>
+              <Questions />
+            </Route>
+            <Route path='/result' exact>
+              <Result />
+            </Route>
+          </Switch>
+        </BrowserRouter>
+      </MainDiv>
     </div>
   );
 }
