8c036cc6674635a69d429120025255c46befb018
[2기-1반] 최연주 <jujubee6323@gmail.com>
2021-08-25 12:57:46 +0900

    redux 파일 분리

--- src/App.js
+++ src/App.js
@@ -9,53 +9,7 @@ import Sample from './pages/Sample';
 import { createStore } from 'redux';
 import { Provider } from 'react-redux';
 import ResultDetail from './pages/ResultDetail';
-
-const initialState = {
-  name: '',
-  gender: '',
-  questions: [],
-  sample: {},
-  answers: {},
-  result: '',
-};
-
-const reducer = (state = initialState, action) => {
-  switch (action.type) {
-    case 'PROFILE_ACTION': {
-      return {
-        ...state,
-        name: action.name,
-        gender: action.gender,
-      };
-    }
-    case 'SAVE_QUESTIONS': {
-      return {
-        ...state,
-        questions: action.payload,
-      };
-    }
-    case 'SAMPLE_INPUT': {
-      return {
-        ...state,
-        sample: { ...state.sample, [action.id]: action.answer },
-      };
-    }
-    case 'ANSWER_INPUTS': {
-      return {
-        ...state,
-        answers: { ...state.answers, [action.id]: action.answer },
-      };
-    }
-    case 'RESULT_INPUT': {
-      return {
-        ...state,
-        result: action.result,
-      };
-    }
-    default:
-      return state;
-  }
-};
+import reducer from './redux';
 
 const store = createStore(reducer);
 
